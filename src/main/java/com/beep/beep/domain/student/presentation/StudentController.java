b3c6087840ce06e9a626f4bd14ba03de5ba717a0
heunseoRyu <heunseo9@gmail.com>
2024-04-20 17:09:02 +0900

    Fix :: StudentController 수정
    요청 추가
    1. studentList 조회
    2. grade로 cls을
    3. grade, cls로
    4. name으로 학생 조회

--- src/main/java/com/beep/beep/domain/student/presentation/StudentController.java
+++ src/main/java/com/beep/beep/domain/student/presentation/StudentController.java
@@ -1,31 +1,40 @@
 package com.beep.beep.domain.student.presentation;
 
 
-import com.beep.beep.domain.student.presentation.dto.request.EnterRoomRequest;
-import com.beep.beep.domain.student.presentation.dto.request.ExitRoomRequest;
+import com.beep.beep.domain.student.presentation.dto.request.AdminStudentResponse;
+import com.beep.beep.domain.student.presentation.dto.response.GetStudentResponse;
+import com.beep.beep.domain.student.presentation.dto.response.SearchStudentResponse;
 import com.beep.beep.domain.student.presentation.dto.response.StudentInfoResponse;
 import com.beep.beep.domain.student.service.StudentService;
+import com.beep.beep.domain.teacher.presentation.dto.response.GetClsResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(value = "/student")
 @RequiredArgsConstructor
 @Tag(name = "Get Info Server (Student)")
 public class StudentController {
 
     private final StudentService studentService;
 
-    @GetMapping()
+    @GetMapping("/students")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get StudentList API")
+    public List<AdminStudentResponse> studentList(){
+        return studentService.studentList();
+    }
+
+    @GetMapping("student")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Get StudentInfo API")
     public StudentInfoResponse getInfo(
@@ -34,24 +43,31 @@ public class StudentController {
         return studentService.getStudentInfo(token);
     }
 
-    @PutMapping("/enter")
-    @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "Entering Room API")
-    public void enterRoom(
-            @RequestHeader(name = "Authorization") String token,
-            @RequestBody EnterRoomRequest request
-    ) {
-        studentService.enter(token,request);
+    @GetMapping("/students/{grade}")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "get Cls API")
+    public List<GetClsResponse> getCls(
+            @PathVariable int grade
+    ){
+        return studentService.getCls(grade);
     }
 
-    @PutMapping("/exit")
-    @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "Exiting Room API")
-    public void exitRoom(
-            @RequestHeader(name = "Authorization") String token,
-            @RequestBody ExitRoomRequest request
-    ) {
-        studentService.exit(token,request);
+    @GetMapping("/students/{grade}/{cls}")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get Students' Info By Grade-Cls API")
+    public List<GetStudentResponse> getStudents(
+            @PathVariable int grade,
+            @PathVariable int cls
+    ){
+        return studentService.getStudents(grade,cls);
     }
 
+    @GetMapping("/students/name")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Search Students API")
+    public List<SearchStudentResponse> searchStudents(
+            @RequestParam String name
+    ){
+        return studentService.searchStudents(name);
+    }
 }
