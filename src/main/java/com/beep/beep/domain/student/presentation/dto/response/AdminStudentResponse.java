4fb1532b9e5644f245f37ef6aaeee77eb5771ccd
heunseoRyu <heunseo9@gmail.com>
2024-05-09 21:43:18 +0900

    Fix :: response 수정
    불필요한 import 삭제

--- src/main/java/com/beep/beep/domain/student/presentation/dto/response/AdminStudentResponse.java
+++ src/main/java/com/beep/beep/domain/student/presentation/dto/response/AdminStudentResponse.java
@@ -1,8 +1,6 @@
 package com.beep.beep.domain.student.presentation.dto.response;
 
 
-import com.beep.beep.domain.student.domain.StudentId;
-import com.beep.beep.domain.user.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
