70875001025bc98ebf6ad0bbe8a3f240b8797a66
heunseoRyu <heunseo9@gmail.com>
2024-05-09 21:40:18 +0900

    Fix :: AuthDetails 수정

--- src/main/java/com/beep/beep/global/security/auth/AuthDetails.java
+++ src/main/java/com/beep/beep/global/security/auth/AuthDetails.java
@@ -1,54 +1,60 @@
 package com.beep.beep.global.security.auth;
 
-
-
-import com.beep.beep.domain.user.domain.User;
+import com.beep.beep.domain.user.presentation.dto.User;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.Collections;
 
 @Getter
-@RequiredArgsConstructor
 public class AuthDetails implements UserDetails {
 
     private final User user;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    private AuthDetails(final User user, final Collection<? extends GrantedAuthority> authorities) {
+        this.user = user;
+        this.authorities = authorities;
+    }
+
+    public AuthDetails(final User user) {
+        this.user = user;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton((GrantedAuthority) () -> user.getAuthority().name());
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return this.user.getPassword();
+        return null;
     }
 
     @Override
     public String getUsername() {
-        return this.user.getEmail();
+        return user.getEmail();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
+
 }
