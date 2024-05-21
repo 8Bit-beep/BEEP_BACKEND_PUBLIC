be1f0d3e6e6bd7fc740cb4c9c380aabd86bfb9af
heunseoRyu <heunseo9@gmail.com>
2024-05-21 21:45:46 +0900

    [Fix] :: test
    just delete error code

--- src/test/java/com/beep/beep/BeepApplicationTests.java
+++ src/test/java/com/beep/beep/BeepApplicationTests.java
@@ -1,13 +1,11 @@
 package com.beep.beep;
 
-import com.beep.beep.domain.user.domain.repository.UserRepository;
-import com.beep.beep.domain.user.exception.UserNotFoundException;
-import com.beep.beep.domain.user.presentation.dto.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BeepApplicationTests {
+
     @Test
 	void contextLoads() {
 	}
