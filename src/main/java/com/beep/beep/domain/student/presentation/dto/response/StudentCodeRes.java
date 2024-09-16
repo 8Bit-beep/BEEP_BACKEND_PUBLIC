586457d45c4b95084455a13a9e9cc359c8819bd9
heunseoRyu <heunseo9@gmail.com>
2024-09-17 01:03:59 +0900

    [fix] :: StudentCodeRes,StudentRes
    remove builder

--- src/main/java/com/beep/beep/domain/student/presentation/dto/response/StudentCodeRes.java
+++ src/main/java/com/beep/beep/domain/student/presentation/dto/response/StudentCodeRes.java
@@ -2,16 +2,12 @@ package com.beep.beep.domain.student.presentation.dto.response;
 
 import com.beep.beep.domain.student.domain.Student;
 import com.beep.beep.domain.student.exception.NotAttendedException;
-import lombok.Builder;
 
 
-@Builder
 public record StudentCodeRes(String code) {
     public static StudentCodeRes of(Student student) {
         try {
-            return StudentCodeRes.builder()
-                    .code(student.getRoom().getCode())
-                    .build();
+            return new StudentCodeRes(student.getRoom().getCode());
         }catch(NullPointerException e){
             throw NotAttendedException.EXCEPTION;
         }
