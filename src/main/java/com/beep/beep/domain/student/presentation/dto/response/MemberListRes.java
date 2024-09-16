21493678870449f9ff9c7ea14f7f02784df6656f
heunseoRyu <heunseo9@gmail.com>
2024-09-17 01:26:55 +0900

    [fix] :: MemberListRes
    fix NullPointException by making default datasource of roomName and floor

--- src/main/java/com/beep/beep/domain/student/presentation/dto/response/MemberListRes.java
+++ src/main/java/com/beep/beep/domain/student/presentation/dto/response/MemberListRes.java
@@ -16,11 +16,19 @@ public record MemberListRes(String name, Integer num, String roomName, Integer f
     }
 
     public static MemberListRes of(Student student) {
+        String roomName = "";
+        Integer floor = -1;
+
+        if(student.getRoom() != null) {
+           roomName = student.getRoom().getName();
+           floor = student.getRoom().getFloor();
+        }
+
         return new MemberListRes(
                 student.getUser().getName(),
                 student.getNum(),
-                student.getRoom().getName(),
-                student.getRoom().getFloor(),
+                roomName,
+                floor,
                 student.getModifiedDate()
         );
     }
