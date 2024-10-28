7333a901fec422e7173eabc8346129a4a2b88f13
heunseoRyu <heunseo9@gmail.com>
2024-10-28 17:08:19 +0900

    [fix] :: https

--- src/main/java/com/beep/beep/BeepApplication.java
+++ src/main/java/com/beep/beep/BeepApplication.java
@@ -1,18 +1,13 @@
 package com.beep.beep;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
-@OpenAPIDefinition(servers = {@Server(url = "https://beep.pe.kr", description = "https 도메인")})
 public class BeepApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/beep/beep/global/config/SwaggerConfig.java
+++ src/main/java/com/beep/beep/global/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.beep.beep.global.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -7,6 +9,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://beep.pe.kr", description = "kopis https 서버입니다."),
+                @Server(url = "http://beep.pe.kr", description = "kopis http 서버입니다."),
+                @Server(url = "http://43.201.252.15", description = "kopis local 서버입니다.")
+        }
+)
 public class SwaggerConfig {
 
     @Bean
