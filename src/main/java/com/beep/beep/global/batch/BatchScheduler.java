26e0b5aadd8c5dd63ef1880b470b7c6cb37d7cee
heunseoRyu <heunseo9@gmail.com>
2025-01-10 02:57:01 +0900

    [fix] :: BatchScheduler, TimeTable
    스케쥴링 타임 변경
    ~~~~오전~~~~
    9시 출석 (1차)
    
    ~~~~오후~~~~
    1시 20분 출석 (2차)
    4시 출석 (3차)
    8시 45분 출석 (4차)

--- src/main/java/com/beep/beep/global/batch/BatchScheduler.java
+++ src/main/java/com/beep/beep/global/batch/BatchScheduler.java
@@ -17,10 +17,28 @@ public class BatchScheduler {
     private final JobLauncher jobLauncher;
     private final Job createRepeatSchedule;
 
-    // 자정 (00:00)
-    @Scheduled(cron = "0 0 0 * * ?")
-    public void runCreateRepeatScheduleAtMidnight() {
-        runJob("00:00 자정",true); // 초기화
+    // 오후 8시 45분 이후 초기화
+    @Scheduled(cron = "0 0 22 * * ?")
+    public void runCreateRepeatScheduleAt10PM() {
+        runJob("22:00 오후 10시",true); // 초기화
+    }
+
+    // 오전 9시 이후 초기화
+    @Scheduled(cron = "0 0 13 * * ?")
+    public void runCreateRepeatScheduleAt1PM() {
+        runJob("13:00 오후 1시",true); // 초기화
+    }
+
+    // 오후 1시 20분 이후 초기화
+    @Scheduled(cron = "0 50 15 * * ?")
+    public void runCreateRepeatScheduleAt350PM() {
+        runJob("15:50 오후 1시",true); // 초기화
+    }
+
+    // 오후 4시 이후 초기화
+    @Scheduled(cron = "0 40 20 * * ?")
+    public void runCreateRepeatScheduleAt840PM() {
+        runJob("20:40 오후 8시 40분",true); // 초기화
     }
 
 //    // 오후 5시 25분 (17:25)
@@ -29,29 +47,29 @@ public class BatchScheduler {
 //        runJob("17:25 오후 5시 25분",false);
 //    }
 
-    // 오후 6시 20분 (18:20)
-    @Scheduled(cron = "0 20 18 * * ?")
-    public void runCreateRepeatScheduleAt620PM() {
-        runJob("18:15 오후 6시 20분",true); // 초기화
-    }
-
-    // 오후 8시 (20:00)
-    @Scheduled(cron = "0 0 20 * * ?")
-    public void runCreateRepeatScheduleAt7PM() {
-        runJob("20:00 오후 8시",false);
-    }
-
-    // 오후 8시 45분 (20:45)
-    @Scheduled(cron = "0 45 20 * * ?")
-    public void runCreateRepeatScheduleAt845PM() {
-        runJob("20:45 오후 8시 45분",true); // 초기화
-    }
-
-    // 오후 9시 30분 (21:30)
-    @Scheduled(cron = "0 30 21 * * ?")
-    public void runCreateRepeatScheduleAt930PM() {
-        runJob("20:45 오후 8시 45분",true); // 초기화
-    }
+//    // 오후 6시 20분 (18:20)
+//    @Scheduled(cron = "0 20 18 * * ?")
+//    public void runCreateRepeatScheduleAt620PM() {
+//        runJob("18:15 오후 6시 20분",true); // 초기화
+//    }
+//
+//    // 오후 8시 (20:00)
+//    @Scheduled(cron = "0 0 20 * * ?")
+//    public void runCreateRepeatScheduleAt7PM() {
+//        runJob("20:00 오후 8시",false);
+//    }
+//
+//    // 오후 8시 45분 (20:45)
+//    @Scheduled(cron = "0 45 20 * * ?")
+//    public void runCreateRepeatScheduleAt845PM() {
+//        runJob("20:45 오후 8시 45분",true); // 초기화
+//    }
+//
+//    // 오후 9시 30분 (21:30)
+//    @Scheduled(cron = "0 30 21 * * ?")
+//    public void runCreateRepeatScheduleAt930PM() {
+//        runJob("20:45 오후 8시 45분",true); // 초기화
+//    }
 
     /**
      * RepeatType에 따른 반복 일정 생성
